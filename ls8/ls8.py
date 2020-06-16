dc18e1fc8aa53331c7d5d20cac93f031a4bb87df
asbaden <gusbaden96@gmail.com>
2020-06-16 14:53:31 -0700

    completed load functiont to orun file names dynamically

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -4,35 +4,33 @@
 import sys
 from cpu import *
 
-mul = [
-    0b10000010, # LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b10000010,# LDI R1,9
-    0b00000001,
-    0b00001001,
-    0b10100010, # MUL R0,R1
-    0b00000000,
-    0b00000001,
-    0b01000111, # PRN R0
-    0b00000000,
-    0b00000001, # HLT
-]
 
-print8 = [
-    0b10000010, # LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111,# PRN R0
-    0b00000000,
-    0b00000001,
 
-]
+
+program = []
+
+print("this is program", program)
+
+
+file_name = sys.argv[1]
+print("this is filename", file_name)
+
+
+
+with open(file_name) as f:
+    lines = f.readlines()
+    for line in lines:
+        if line[0]!= '#':
+            num = int(line[0:8], 2)
+            program.append(num)
+    print(program)
+
+
 
 
 # sys.path.append("/examples")
 
 cpu = CPU()
 
-cpu.load(mul)
+cpu.load(program)
 cpu.run()
